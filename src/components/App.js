460fc825aa0f0cf5c8f6944327f96fcd5ecf0b56
Emily M <emilymwendwa390@gmail.com>
2024-11-04 15:18:29 +0300

    imports hogtile

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,8 @@ import Nav from "./Nav";
 
 import hogs from "../porkers_data";
 import React, { useState } from "react";
-import hogsData from "./porkers_data"; // Import the hog data
+import hogsData from "./porkers_data"; // Imports the hog data
+import hogTile from "./hogTile"; //imports the hogTile component
 
 function App() {
 	const [hogs, setHogs] = useState(hogsData); 	//Sets up state for allhogs
@@ -11,9 +12,22 @@ function App() {
 	const [sortType, setSortType] = useState("name"); // sorts by name or weight
 	const [hiddenHogs, setHiddenHogs] = useState([]); // Tracks hidden hogs
 	
+
+	//filters and sorts hogs array
+	const displayHogs = hogs
+	.filter(hog => !hiddenHogs.includes(hog.name))
+	.filter(hog => !filterGreased || hog.greased)
+	.sort((a,b) => {
+		if (sortType === "name") return a.name.localeCompare(b.name);
+		if (sortType === "weight") return a.weight - b.weight;
+	});
+
 	//renders the APP for the component
 	return (
 		<div className="App">
+			{displayHogs.map(hog => (
+				<hogTile key={hog.name} hog={hog} />
+			))}
 			<Nav />
 		</div>
 	);
--- src/components/hogTile.js
+++ src/components/hogTile.js
@@ -1,1 +1,22 @@
 import React, {useState} from "react";
+function hogTile({ hog }) {
+    const [showDetails, setShowDetails] = useState(false);
+    //Toggles details display
+    const handleClick = () => setShowDetails(!showDetails);
+
+    return(
+        <div className="App" onClick={handleClick}>
+            <h3>{hog.name}</h3>
+            <img src={hog.name} alt={hog.name} />
+            {showDetails && (
+                <div>
+                    <p>Specialty: {hog.specialty}</p>
+                    <p>Weight: {hog.weight}</p>
+                    <p>Greased: {hog.greased ? "Yes" : "No"}</p>
+                    <p>Highest Medal: {hog["highest medal achieved"]}</p>
+                    </div>
+            )}
+        </div>
+    );
+}
+export default hogTile;
